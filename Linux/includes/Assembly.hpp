49cba91e89b9f82fc6bfa1823d7cbd1ed48ea363
Joris Bertomeu <joris.bertomeu@epitech.eu>
2016-01-14 16:00:21 +0100

    Assembly.hpp

---
+++ Linux/includes/Assembly.hpp
@@ -0,0 +1,42 @@
+#ifndef		_ASSEMBLY_HPP_
+# define	_ASSEMBLY_HPP_
+
+# include	<string>
+
+class		Assembly
+{
+private:
+  std::string	_version;
+  std::string	_author;
+  std::string	_name;
+  std::string	_updateURL;
+  
+public:
+  void		set(const std::string &version, const std::string &author, const std::string &name) {
+    this->_version = version;
+    this->_author = author;
+    this->_name = name;
+  };
+
+  std::string	getVersion() const {
+    return (_version);
+  };
+
+  std::string	getAuthor() const {
+    return (_author);
+  };
+
+  std::string	getName() const {
+    return (_name);
+  };
+
+  void		setUpdateURL(const std::string &url) {
+    this->_updateURL = url;
+  };
+
+  void		checkForUpdate() {
+
+  };
+};
+
+#endif
